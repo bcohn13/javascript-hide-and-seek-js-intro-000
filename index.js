87486bb25c526df3d3722c05dfb971468c100544
Benjamin Cohn <cohnbj13@gmail.com>
2020-05-22 17:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget() {
+function nestedTarget(selector) {
 
-  return document.querySelectorAll("tr")
+  return document.querySelectorAll(selector)
 }
